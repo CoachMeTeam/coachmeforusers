e0318b267749e53864d04e51081edc6a31988fd3
Arno Da Silva <arnodslv@gmail.com>
2017-01-21 15:40:34 +0100

    feat(properties): adding of a config properties reader

---
+++ app/src/main/java/com/coachme/coachmeforusers/utils/ConfigPropertiesReader.java
@@ -0,0 +1,20 @@
+package com.coachme.coachmeforusers.utils;
+
+
+
+import com.coachme.coachmeforusers.CoachMeForAdminApp;
+
+import java.util.Properties;
+
+public class ConfigPropertiesReader {
+    public static Properties getConfig() {
+        try {
+            Properties properties = new Properties();
+            properties.load(CoachMeForAdminApp.getContext().getAssets().open("app.properties"));
+            return properties;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
