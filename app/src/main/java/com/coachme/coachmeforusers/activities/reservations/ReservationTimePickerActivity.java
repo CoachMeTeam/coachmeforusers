4a2b4447722f5e176aecdddafdaaead8021eb766
Arno Da Silva <arnodslv@gmail.com>
2017-01-25 15:02:00 +0100

    feat(timePicker): sending of reservation requests

--- app/src/main/java/com/coachme/coachmeforusers/activities/reservations/ReservationTimePickerActivity.java
+++ app/src/main/java/com/coachme/coachmeforusers/activities/reservations/ReservationTimePickerActivity.java
@@ -6,8 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.coachme.coachmeforusers.CoachMeForAdminApp;
 import com.coachme.coachmeforusers.R;
+import com.coachme.coachmeforusers.model.Machine;
+import com.coachme.coachmeforusers.model.Reservation;
+import com.coachme.coachmeforusers.utils.Helper;
+
+import org.restlet.data.MediaType;
+import org.restlet.ext.json.JsonRepresentation;
+import org.restlet.resource.ClientResource;
+import org.restlet.resource.ResourceException;
+
+import java.util.Date;
+
+import static com.coachme.coachmeforusers.utils.Helper.API_ENDPOINT;
 
 public class ReservationTimePickerActivity extends Activity {
     private int newReservationTimeSelected = 0;
@@ -20,11 +34,11 @@ public class ReservationTimePickerActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_picker);
+
+        initComponents();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    private void initComponents() {
         startTrainingButton = (Button) findViewById(R.id.startTrainingButton);
         timePickerLessButton = (Button) findViewById(R.id.timePickerLessButton);
         timePickerPlusButton = (Button) findViewById(R.id.timePickerPlusButton);
@@ -36,9 +50,27 @@ public class ReservationTimePickerActivity extends Activity {
         startTrainingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ReservationTimeRemainingActivity.class);
-                intent.putExtra("newReservationTimeSelected", Integer.toString(newReservationTimeSelected));
-                startActivity(intent);
+                ClientResource reservationResource = new ClientResource(API_ENDPOINT + "/reservations");
+                String stringMachine = Helper.getSharedPreference("currentMachine");
+                Machine machine = Helper.convertJsonToObject(stringMachine, Machine.class);
+                String reservationDate = Helper.getStringDate(new Date());
+                int duration = newReservationTimeSelected * 60;
+
+                Reservation reservation = new Reservation(reservationDate, duration, machine.getId(),
+                        CoachMeForAdminApp.getCurrentUser().getId());
+                String jsonReservation = Helper.convertObjectToJson(reservation);
+                try {
+                    reservationResource.post(new JsonRepresentation(jsonReservation), MediaType.APPLICATION_JSON);
+
+                    Intent intent = new Intent(getApplicationContext(), ReservationTimeRemainingActivity.class);
+                    intent.putExtra("newReservationTimeSelected", Integer.toString(newReservationTimeSelected));
+                    startActivity(intent);
+                } catch (ResourceException e) {
+                    Toast.makeText(getApplicationContext(),
+                            "Une erreur est survenue lors du choix de temps d'entrainement.",
+                            Toast.LENGTH_LONG)
+                            .show();
+                }
             }
         });
 
@@ -56,18 +88,13 @@ public class ReservationTimePickerActivity extends Activity {
             @Override
             public void onClick(View v) {
                 int currentReservationTimeSelected = Integer.parseInt(timePickerTextView.getText().toString());
-                newReservationTimeSelected = currentReservationTimeSelected + 1; // A voir s'il ne faut pas limiter le temps maximum de reservation
+                newReservationTimeSelected = currentReservationTimeSelected + 1;
                 timePickerTextView.setText(Integer.toString(newReservationTimeSelected));
                 setStartTrainingButtonAvailability();
             }
         });
     }
 
-    /**
-     * Cette methode active ou désactive le bouton permettant le démarrage de l'entrainement suivant le temps d'entrainement choisi
-     * Si le temps d'entrainement est égal à 0 alors le bouton est désactivé
-     * Sinon si le temps d'entrainement est supérieur à O alors le bouton est activé
-     */
     private void setStartTrainingButtonAvailability() {
         if (newReservationTimeSelected == 0) {
             startTrainingButton.setEnabled(false);
